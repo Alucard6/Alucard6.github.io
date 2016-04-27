a06f4da24a82440ba4be6a31dcd123678cc4e059
Senn <csag7723@uibk.ac.at>
2016-04-27 13:00:47 +0200

    nochmals ausgebessert

--- adler/adler.js
+++ adler/adler.js
@@ -1,95 +1,103 @@
+//Adlerweg Skript
+//zeigt vier etappen des Adlerwegs auf einer Leaflet Karte mit Photos von panoramio
 
-	var adlerKarte = L.map("adlerkarteDiv", { zoom : 9});
-	var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap contributors</a>'
-        }).addTo(adlerKarte);
+/*
+so macht an mehrzeilige Kommentare,
+man könnte also hier weiterschreiben,
+wenn man möchte
+*/
 
-	adlerKarte.setView([47, 11], 10);
-	var etappe01 = L.geoJson(etappe01json, {
-	style: {
-			color: "red",
-			weight: 5}
-	}) .bindPopup ('Etappe01: Die erste Etappe am Adlerweg fuehrt vom St. Johanner Ortsteil Hinterkaiser zur Gaudeamushuette auf 1.263 Metern Seehoehe. Zei besondere Naturschauspiele entlang der Strecke sind der imposante Schleierwasserfall und die Hoehle Diebsoefen.');
-	var etappe02 = L.geoJson(etappe02json, {
-	style: {
-			color: "purple",
-			weight: 5}
-	}) .bindPopup ('Etappe02: Diese Etappe fuehrt ausgehend von der Gaudeamushuette ueber den Klammlweg zu einem besonderem Ziel entlang des Adlerwegs: dem Hintersteiner See. Der Bergsee lockt vor allem im Hochsommer mit Badefreuden.')
-	var etappe03 = L.geoJson(etappe03json, {
-	style: {
-			color: "blue",
-			weight: 5}
-	}) .bindPopup ('Etappe03: Die dritte Etappe am Adlerweg fuehrt vom Hintersteiner See ins Staedtchen Kufstein. Vom 1.470 Meter hohen Hochegg fuehrt die sanfte Wanderung abwaerts in Richtung Kufstein, wo man die imposante Festung Kufstein besichtigen kann.')
-	var etappe04 = L.geoJson(etappe04json, {
-	style: {
-			color: "green",
-			weight: 5}
-	}) .bindPopup ('Etappe04: Nicht ganz ohne Schweiss sind die mehr als 1.300 Hoehenmeter dieser Adlerweg-Etappe zu bewaeltigen. Aber es lohnt sich! Am Ende der Strecke wartet ein besonderes Naturschauspiel: die Hundsalm-Eishoehle, eine auf knapp 1.400 Meter liegende Eis- und Tropfsteinhoehle.')
-	
-	var etappenGruppe = L.featureGroup([etappe01, etappe02, etappe03, etappe04])
-	
-	adlerKarte.addLayer(etappenGruppe);
-	
-	adlerKarte.fitBounds(etappenGruppe.getBounds());
+var adlerKarte = L.map("adlerkarteDiv", { zoom : 9});
+var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap contributors</a>'
+	}).addTo(adlerKarte);
 
-	
-	window.onload = function () {
-	
-        var layers = { // http://www.basemap.at/wmts/1.0.0/WMTSCapabilities.xml
-            geolandbasemap : L.tileLayer("http://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
-                subdomains : ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
-                attribution : 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
-            }),
-            bmapoverlay : L.tileLayer("http://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
-                subdomains : ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
-                attribution : 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
-            }),
-            bmapgrau : L.tileLayer("http://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
-                subdomains : ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
-                attribution : 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
-            }),
-            bmaphidpi : L.tileLayer("http://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
-                subdomains : ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
-                attribution : 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
-            }),
-            bmaporthofoto30cm : L.tileLayer("http://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
-                subdomains : ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
-                attribution : 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
-            })
-        };
-            L.control.layers({
-			"OSM": osmLayer,
-			"Tracks": etappenGruppe,
-            "Geoland Basemap" : layers.geolandbasemap,
-            "Geoland Basemap Overlay" : layers.bmapoverlay,
-            "Geoland Basemap Grau": layers.bmapgrau,
-            "Geoland Basemap High DPI" : layers.bmaphidpi,
-            "Geoland Basemap Orthofoto" : layers.bmaporthofoto30cm
-        }).addTo(adlerKarte)
-        L.control.scale({
-            'imperial' : false
-        }).addTo(adlerKarte);}
-	var bounds = etappenGruppe.getBounds();
-	var url = 'http://www.panoramio.com/map/get_panoramas.php?set=public&from=0&to=20'
-		+'&minx='+bounds.getWest()
-		+'&miny='+bounds.getSouth()
-		+'&maxx='+bounds.getEast()
-		+'&maxy='+bounds.getNorth()
-		+'&size=mini_square&mapfilter=true&callback=zeigBilder';
-	
-	var script = document.createElement('script');
-	script.src = url;
-	document.getElementsByTagName('head')[0].appendChild(script);
-	window.zeigBilder = function(data) {
-	for (var i=0; i<data.photos.length; i++) {
-	console.log('Photo title:', i, data.photos[i].photo_title);
-	L.marker([data.photos[i].latitude, data.photos[i].longitude],
-	{icon: L.icon({
-			iconUrl: data.photos[i].photo_file_url
-			})
-	}).bindPopup('<h2>'+data.photos[i].photo_title+'</2>'
-	+'<a href="'+data.photos[i].photo_url+'">Link zu Panoramio</a>')
-	.addTo(adlerKarte);
+adlerKarte.setView([47, 11], 10);
+var etappe01 = L.geoJson(etappe01json, {
+style: {
+		color: "red",
+		weight: 5}
+}) .bindPopup ('Etappe01: Die erste Etappe am Adlerweg fuehrt vom St. Johanner Ortsteil Hinterkaiser zur Gaudeamushuette auf 1.263 Metern Seehoehe. Zei besondere Naturschauspiele entlang der Strecke sind der imposante Schleierwasserfall und die Hoehle Diebsoefen.');
+var etappe02 = L.geoJson(etappe02json, {
+style: {
+		color: "purple",
+		weight: 5}
+}) .bindPopup ('Etappe02: Diese Etappe fuehrt ausgehend von der Gaudeamushuette ueber den Klammlweg zu einem besonderem Ziel entlang des Adlerwegs: dem Hintersteiner See. Der Bergsee lockt vor allem im Hochsommer mit Badefreuden.')
+var etappe03 = L.geoJson(etappe03json, {
+style: {
+		color: "blue",
+		weight: 5}
+}) .bindPopup ('Etappe03: Die dritte Etappe am Adlerweg fuehrt vom Hintersteiner See ins Staedtchen Kufstein. Vom 1.470 Meter hohen Hochegg fuehrt die sanfte Wanderung abwaerts in Richtung Kufstein, wo man die imposante Festung Kufstein besichtigen kann.')
+var etappe04 = L.geoJson(etappe04json, {
+style: {
+		color: "green",
+		weight: 5}
+}) .bindPopup ('Etappe04: Nicht ganz ohne Schweiss sind die mehr als 1.300 Hoehenmeter dieser Adlerweg-Etappe zu bewaeltigen. Aber es lohnt sich! Am Ende der Strecke wartet ein besonderes Naturschauspiel: die Hundsalm-Eishoehle, eine auf knapp 1.400 Meter liegende Eis- und Tropfsteinhoehle.')
 
-	}}
- 
+var etappenGruppe = L.featureGroup([etappe01, etappe02, etappe03, etappe04])
+
+adlerKarte.addLayer(etappenGruppe);
+
+adlerKarte.fitBounds(etappenGruppe.getBounds());
+
+
+window.onload = function () {
+
+	var layers = { // http://www.basemap.at/wmts/1.0.0/WMTSCapabilities.xml
+		geolandbasemap : L.tileLayer("http://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+			subdomains : ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
+			attribution : 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
+		}),
+		bmapoverlay : L.tileLayer("http://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
+			subdomains : ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
+			attribution : 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
+		}),
+		bmapgrau : L.tileLayer("http://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
+			subdomains : ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
+			attribution : 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
+		}),
+		bmaphidpi : L.tileLayer("http://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
+			subdomains : ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
+			attribution : 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
+		}),
+		bmaporthofoto30cm : L.tileLayer("http://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
+			subdomains : ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
+			attribution : 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
+		})
+	};
+		L.control.layers({
+		"OSM": osmLayer,
+		"Tracks": etappenGruppe,
+		"Geoland Basemap" : layers.geolandbasemap,
+		"Geoland Basemap Overlay" : layers.bmapoverlay,
+		"Geoland Basemap Grau": layers.bmapgrau,
+		"Geoland Basemap High DPI" : layers.bmaphidpi,
+		"Geoland Basemap Orthofoto" : layers.bmaporthofoto30cm
+	}).addTo(adlerKarte)
+	L.control.scale({
+		'imperial' : false
+	}).addTo(adlerKarte);}
+var bounds = etappenGruppe.getBounds();
+var url = 'http://www.panoramio.com/map/get_panoramas.php?set=public&from=0&to=20'
+	+'&minx='+bounds.getWest()
+	+'&miny='+bounds.getSouth()
+	+'&maxx='+bounds.getEast()
+	+'&maxy='+bounds.getNorth()
+	+'&size=mini_square&mapfilter=true&callback=zeigBilder';
+
+var script = document.createElement('script');
+script.src = url;
+document.getElementsByTagName('head')[0].appendChild(script);
+window.zeigBilder = function(data) {
+for (var i=0; i<data.photos.length; i++) {
+console.log('Photo title:', i, data.photos[i].photo_title);
+L.marker([data.photos[i].latitude, data.photos[i].longitude],
+{icon: L.icon({
+		iconUrl: data.photos[i].photo_file_url
+		})
+}).bindPopup('<h2>'+data.photos[i].photo_title+'</2>'
++'<a href="'+data.photos[i].photo_url+'">Link zu Panoramio</a>')
+.addTo(adlerKarte);
+
+}}
+
